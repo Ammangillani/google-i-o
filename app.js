54a6197487c7791bcab11f8121157b97f3bf1106
Amman Gillani <ammangillani4321@icloud.com>
2024-04-16 13:35:18 +0500

    code

---
+++ app.js
@@ -0,0 +1,22 @@
+let daycount = new Date("may 15, 2024 00:07:00").getTime();
+
+let count = setInterval(function () {
+  let now = new Date().getTime();
+  let time = daycount - now;
+
+  let days = Math.floor(time / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  // let minutes = Math.floor(time / (1000 * 60 * 60 * 24));
+  let second = Math.floor((time % (1000 * 60)) / 1000);
+
+  document.getElementById("day").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("secondes").innerHTML = second;
+
+  if (time < 0) {
+    clearInterval(count);
+    document.getElementById;
+  }
+});
