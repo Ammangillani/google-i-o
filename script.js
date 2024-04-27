a518571483f19988e7659686ae75a8f112a9b3d6
Amman Gillani <ammangillani4321@icloud.com>
2024-04-28 01:10:52 +0500

    cc

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const sections = document.querySelectorAll(".section");
-  const navLinks = document.querySelectorAll(".nav-link");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const sections = document.querySelectorAll(".section");
+//   const navLinks = document.querySelectorAll(".nav-link");
 
-  window.addEventListener("scroll", function () {
-    let current = "";
-    sections.forEach((section) => {
-      const sectionTop = section.offsetTop;
-      const sectionHeight = section.clientHeight;
-      if (pageYOffset >= sectionTop - sectionHeight / 6) {
-        current = section.getAttribute("id");
-      }
-    });
+//   window.addEventListener("scroll", function () {
+//     let current = "";
+//     sections.forEach((section) => {
+//       const sectionTop = section.offsetTop;
+//       const sectionHeight = section.clientHeight;
+//       if (pageYOffset >= sectionTop - sectionHeight / 6) {
+//         current = section.getAttribute("id");
+//       }
+//     });
 
-    navLinks.forEach((link) => {
-      link.classList.remove("active");
-      if (link.getAttribute("href").includes(current)) {
-        link.classList.add("active");
-      }
-    });
-  });
-});
+//     navLinks.forEach((link) => {
+//       link.classList.remove("active");
+//       if (link.getAttribute("href").includes(current)) {
+//         link.classList.add("active");
+//       }
+//     });
+//   });
+// });
