df09e3c970cb417f4caa443b890cdf1ceb4bcac4
Amman Gillani <ammangillani4321@icloud.com>
2024-05-08 02:14:51 +0500

    commit

--- hemberg.js
+++ hemberg.js
@@ -1,16 +1,10 @@
-let close = document.getElementById("close");
-let open = document.getElementById("open");
-let nav = document.querySelector(".nav-content");
-let spanclose = document.getElementById("spanclose");
+function openMenu() {
+  document.getElementById("mobileMenu").classList.add("show");
+}
 
-function openmenu() {
-  nav.style.right = "20px";
-  open.style.display = "none";
-  spanclose.style.display = "block";
+function closeMenu() {
+  document.getElementById("mobileMenu").classList.remove("show");
 }
-function closemenu() {
-  nav.style.right = "-1000px";
-  open.style.display = "block";
-  spanclose.style.display = "none";
-}
-close.addEventListener("click", closemenu);
+
+document.getElementById("openMenu").addEventListener("click", openMenu);
+document.getElementById("closeMenu").addEventListener("click", closeMenu);
