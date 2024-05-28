15cff444d0ad6e5a0dd78e92aa2c4565d1e05f58
Amman Gillani <ammangillani4321@icloud.com>
2024-05-28 15:54:14 +0500

    commit

--- register.js
+++ register.js
@@ -1,21 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyANg8sFbgQaDoYbiWa6Da1XWp7gWl1558k",
-  authDomain: "io-goggle.firebaseapp.com",
-  projectId: "io-goggle",
-  storageBucket: "io-goggle.appspot.com",
-  messagingSenderId: "50870650879",
-  appId: "1:50870650879:web:626d920affd17e0c6d09ca",
-  measurementId: "G-0GJFVY54JW",
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyANg8sFbgQaDoYbiWa6Da1XWp7gWl1558k",
+//   authDomain: "io-goggle.firebaseapp.com",
+//   projectId: "io-goggle",
+//   storageBucket: "io-goggle.appspot.com",
+//   messagingSenderId: "50870650879",
+//   appId: "1:50870650879:web:626d920affd17e0c6d09ca",
+//   measurementId: "G-0GJFVY54JW",
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
